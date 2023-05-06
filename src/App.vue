7870328eb9ace7e59f5be5740f35cbecca6c2b7c
HAN-homePC <thepub03@gmail.com>
2023-05-06 22:40:38 +0900

    kit setup

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,28 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Button :label="args.label" :primary=args.primary />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Button from './stories/Button.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Button
+  },
+  data() {
+    return {
+      args: {
+        primary: true,
+        label: 'Button11'
+      }
+    }
   }
 }
 </script>
